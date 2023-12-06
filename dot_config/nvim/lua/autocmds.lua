31c5fd7c0d449da9898d0b2de738994fea2d2561
kwhtsng <deep.bolt2mann.machine@gmail.com>
2023-12-06 17:07:45 +0900

    Update .config/nvim/lua/autocmds.lua

--- dot_config/nvim/lua/autocmds.lua
+++ dot_config/nvim/lua/autocmds.lua
@@ -82,6 +82,7 @@ autocmd({ "FileType" }, {
 autocmd({ "FileType" }, {
 	pattern = { "yaml", "yml" },
 	callback = function()
+		vim.opt_local.tabstop = 2
 		vim.opt_local.shiftwidth = 2
 		vim.opt_local.softtabstop = 2
 		vim.opt_local.expandtab = true
